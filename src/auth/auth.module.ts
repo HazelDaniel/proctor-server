ddd509cb4dca5cb2f120c537425bb4d1ce2d5e9c
HazelDaniel <hazeldaniel856@gmail.com>
2026-02-12 11:11:14 +0100

    fix(auth) passwordless auth feature text expansion db initialization fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { AuthResolver } from '../api/contracts/graphql/resolvers/auth.resolver';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET!,
-      signOptions: { algorithm: 'HS256' }, // adjust if you use RS256
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const secret = config.get<string>('JWT_SECRET');
+        if (!secret) throw new Error('JWT_SECRET not found in environment');
+        return {
+          secret,
+          signOptions: { algorithm: 'HS256', expiresIn: '7d' },
+        };
+      },
     }),
   ],
   providers: [AuthService, AuthResolver],
