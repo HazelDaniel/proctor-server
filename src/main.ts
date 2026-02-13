ba8abee4ed2dce2a17ca387416730a8ef275d165
HazelDaniel <hazeldaniel856@gmail.com>
2026-02-13 13:51:05 +0100

    chore: install package, lock in MVP

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Proctor API')
